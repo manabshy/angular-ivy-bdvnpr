ad172b5398c47346ef08ac1f7dd5cdc5d105cce8
manabshy <manoj.bisht@chatresh.co.uk>
2021-10-13 21:11:09 +0100

    4 files modified

--- src/app/simple-form.component.ts
+++ src/app/simple-form.component.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Title } from './title.model';
+import { TitleService } from './title.service';
 
 @Component({
   selector: 'simple-form',
   templateUrl: './simple-form.component.html',
-  styleUrls: ['./simple-form.component.css']
+  styleUrls: ['./simple-form.component.css'],
 })
-export class SimpleFormComponent {}
+export class SimpleFormComponent implements OnInit {
+  titles: Title[];
+  form: FormGroup;
+
+  constructor(private titleService: TitleService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.getTitles();
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      title: new FormControl(''),
+      firstName: new FormControl(''),
+      lastName: new FormControl('', Validators.required),
+      acceptTerms: new FormControl(''),
+    });
+  }
+
+  getTitles() {
+    this.titleService.getTitles().subscribe((response) => {
+      this.titles = response.filter((item) => item.name !== '!');
+      this.titles.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+    });
+  }
+
+  submit() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
--- src/app/title.service.ts
+++ src/app/title.service.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Title } from './title.model';
 
+@Injectable({ providedIn: 'root' })
 export class TitleService {
   getTitles(): Observable<Title[]> {
     // assume this data is coming from a backend data source and cannot be changed
@@ -10,7 +12,7 @@ export class TitleService {
       { name: 'Miss', isDefault: false },
       { name: '!', isDefault: false },
       { name: 'Dr', isDefault: true },
-      { name: 'Prof', isDefault: false }
+      { name: 'Prof', isDefault: false },
     ]);
   }
 }
